a2f2e31afbdfd43a830040173ac546ee713ba242
odysseu <uboucherie1@gmail.com>
2022-04-20 13:47:11 +0000

    Added class table, class Cell, imports.py

---
+++ code/Table.py
@@ -0,0 +1,6 @@
+class Table
+    def __init__(self, length, height):
+        self.lines = np.array(String)
+        self.length = length
+        self.height = height
+        
---
+++ code/main.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    # Import useful packages
+    import imports
+    import Table
+    
+    # laucnh app
+    table1 = Table(12,34)
